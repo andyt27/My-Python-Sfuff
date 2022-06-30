3cec1f23f06e33283803cc3713025c261698794b
AndyTeng <andy@gmail.com>
2022-06-30 14:14:31 -0400

    homework

---
+++ homework_class_5_2.py
@@ -0,0 +1,12 @@
+Sum = 0
+Count = 0
+fname = input('Enter File Name:')
+fh = open(fname)
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:")
+        continue
+    if line.startswith('X-DSPAM-Confidence:')
+         a = float(line[20:])
+         Sum = Sum + a
+         Count = Count + 1
+print('Average spam confidence:', big/Count)
