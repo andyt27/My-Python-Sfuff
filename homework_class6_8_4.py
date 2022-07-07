581741786d729c7195265b58b444ca2c135a5100
AndyTeng <andy@gmail.com>
2022-07-07 18:13:46 -0400

    homework

---
+++ homework_class6_8_4.py
@@ -0,0 +1,13 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+    for line in fh:
+        la=line.rstrip()
+        words=la.split()
+    for i in words:
+    if i in lst :
+        continue
+    else :
+    lst.append(i)
+    lst.sort()
+print(lst)
