f5ce6339e1a88f0cac073e6a449446511c40de6f
AndyTeng <andy@gmail.com>
2022-07-14 19:08:15 -0400

    homework

---
+++ homework_class_7_9.4.py
@@ -0,0 +1,19 @@
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+lst = list()
+for line in handle:
+    if not line.startswith("From "): continue
+    line = line.split()
+    st.append(line[1])
+ counts = dict()
+ for word in lst:
+    counts[word] = counts.get(word,0) + 1
+
+bcount = None
+Bword = None
+for word,count in counts.items():
+if bcount is None or count > bcount:
+bcount = count
+bword = word
